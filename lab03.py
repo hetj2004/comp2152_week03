366b459cf02edb7dee1e4ac57ebc90872ba3778d
hetj2004 <het28jasani.com>
2025-01-24 09:48:51 -0500

    add lab03.py

--- lab03.py
+++ lab03.py
@@ -1,1 +1,49 @@
-print("Hello, World! ")
+import random
+# Dice options using lisy() and range()
+diceOptions = list(range(1,7))
+
+# Weapons Array
+weapons = ["Fist", "Knife", "Club", "Bomb", "Nuclear Bomb"]
+
+# Dsiplay avaliable weapons
+print("Available Weapons: ", ', '.join(weapons))
+
+
+def get_combat_stregth(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if 1 <= value <= 6:
+                return value
+            else:
+                print("Invalid input! Please enter a number between 1-6.")
+        except ValueError:
+            print("Invalid input! Please enter a number between 1-6")
+
+combatStrength = get_combat_stregth("Enter your combat strength (1-6): ")
+mCombatStrength = get_combat_stregth("Enter monster's combat strength (1-6): ")
+
+for j in range(1, 21, 2):
+    heroRoll = random.choice(diceOptions)
+    monsterRoll = random.choice(diceOptions)
+
+    heroWeapon = weapons[heroRoll - 1]
+    monsterWeapon = weapons[monsterRoll - 1]
+
+    heroTotal = combatStrength + heroRoll
+    monsterTotal = mCombatStrength + monsterRoll
+
+    print(f"\n Hero rolled {heroRoll}, Monster rolled {monsterRoll}")
+    print(f"\n Hero selected {heroWeapon}, Monster selected {monsterWeapon}")
+    print(f"\n Hero total Stregth: {heroTotal}, Monster total Stregth: {monsterTotal}")
+
+    # Determine the winner
+    if heroTotal > monsterTotal:
+        print("Hero wins!")
+    elif heroTotal < monsterTotal:    
+        print("Monster wins!")
+    else:
+        print("It's a tie!")
+    
+if j != 11:
+    print(f"\n Battle concluded after 20 rounds!") 
